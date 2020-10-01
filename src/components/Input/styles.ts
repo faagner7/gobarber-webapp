da8ae886d701c9af0d77b74d2277fbf5b1330bb1
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2020-10-01 20:07:58 -0300

    add Yup to project

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background-color: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
@@ -13,6 +18,13 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9900;
+      border-color: #ff9900;
+    `}
 
   input {
     flex: 1;
@@ -28,5 +40,18 @@ export const Container = styled.div`
   svg {
     margin-right: 16px;
     color: #666360;
+
+    ${props =>
+      props.isFocused &&
+      css`
+        color: #ff9900;
+        border-color: #ff9900;
+      `}
+
+    ${props =>
+      props.isFilled &&
+      css`
+        color: #ff9900;
+      `}
   }
 `;
