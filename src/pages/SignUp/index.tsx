63e0ed2e9fb32603a50874ba8f9a69b560803ac1
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2020-10-01 14:15:26 -0300

    add unform to project

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Form } from '@unform/web';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
@@ -10,6 +11,10 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <>
       <Container>
@@ -18,7 +23,7 @@ const SignUp: React.FC = () => {
         <Content>
           <img src={logoImg} alt="GoBarber" />
 
-          <form>
+          <Form onSubmit={handleSubmit}>
             <h1>Faça seu cadastro</h1>
             <Input name="name" icon={FiUser} placeholder="Nome" type="text" />
             <Input
@@ -37,7 +42,7 @@ const SignUp: React.FC = () => {
             <Button type="submit" onClick={() => console.log('clicado')}>
               Cadastrar
             </Button>
-          </form>
+          </Form>
 
           <a href="login">
             <FiArrowLeft />
