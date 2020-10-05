4542ee5c325a17c760e9fdf8cb9423ff1528920c
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2020-10-05 14:15:51 -0300

    add authenticate with api

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import GlobalStyles from './styles/global';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
+
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    {/* <SignUp /> */}
-    <SignIn />
+    <AuthProvider>
+      {/* <SignUp /> */}
+      <SignIn />
+    </AuthProvider>
+
     <GlobalStyles />
   </>
 );
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,57 @@
+import React, { createContext, useCallback, useState } from 'react';
+import api from '../services/api';
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credentials: SignInCredentials): Promise<void>; // Quando passar nosso método para async, o retorno é sempre uma promise!
+}
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+// Forçando o objeto a iniciar vazio com o "as AuthContext",
+// pois o Typescript acusa erro se usarmos um objeto vazio {};
+
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('/sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
